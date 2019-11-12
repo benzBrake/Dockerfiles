f29b84cb3f1d6abb63096510903a39a9ba24a6b1
Ryan Lieu <github-benzbrake@woai.ru>
2019-11-12 22:11:51 +0800

    上传 latest 镜像

--- hooks/build.sh
+++ hooks/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-if [[ $LATEST = "true" ]]; then
-	docker build -l -t ${DOCKER_BUILD} ${BUILD_DIRECTORY}
+if [[ ${TAG_LATEST} = "true" ]]; then
+	docker build -l -t ${DOCKER_REPO}:${DOCKER_TAG} ${BUILD_DIRECTORY}
 else
-	docker build -t ${DOCKER_BUILD} ${BUILD_DIRECTORY}
+	docker build -t ${DOCKER_REPO}:${DOCKER_TAG} ${BUILD_DIRECTORY}
 fi
+docker images
