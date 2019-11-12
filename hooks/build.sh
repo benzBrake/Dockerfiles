ea02a61cc59d4909054401ebdb129ebdf142965f
Ryan Lieu <github-benzbrake@woai.ru>
2019-11-12 22:15:44 +0800

    不知为何不支持 -l 参数

--- hooks/build.sh
+++ hooks/build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
+docker build -t ${DOCKER_REPO}:${DOCKER_TAG} ${BUILD_DIRECTORY}
 if [[ ${TAG_LATEST} = "true" ]]; then
-	docker build -l -t ${DOCKER_REPO}:${DOCKER_TAG} ${BUILD_DIRECTORY}
-else
-	docker build -t ${DOCKER_REPO}:${DOCKER_TAG} ${BUILD_DIRECTORY}
+	docker tag ${DOCKER_REPO}:${DOCKER_TAG} ${DOCKER_REPO}:latest
 fi
 docker images
