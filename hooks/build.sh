a7700b8232a26725340571b17e4c10f902cd0e33
Ryan Lieu <github-benzbrake@woai.ru>
2019-11-13 00:11:05 +0800

    新增buildarg自定义

--- hooks/build.sh
+++ hooks/build.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
-docker build -t ${DOCKER_REPO}:${DOCKER_TAG} ${BUILD_DIRECTORY}
-if [[ ${TAG_LATEST} = "true" ]]; then
-	docker tag ${DOCKER_REPO}:${DOCKER_TAG} ${DOCKER_REPO}:latest
+# Load build args
+if [[ ! -z ${BUILD_ARGS_FILE} ]] && [[ -f ${BUILD_DIRECTORY}/${BUILD_ARGS_FILE} ]]; then
+    BUILD_ARGS=$(cat ${BUILD_DIRECTORY}/${BUILD_ARGS_FILE} | sed "s#^#--build-arg #")
 fi
+# Build image
+docker build -t ${DOCKER_REPO}:${DOCKER_TAG} ${BUILD_ARGS} ${BUILD_DIRECTORY}
+# Tag image
+if [[ ${TAG_LATEST} = "true" ]]; then
+    docker tag ${DOCKER_REPO}:${DOCKER_TAG} ${DOCKER_REPO}:latest
+fi
+# List image
 docker images
