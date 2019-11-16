a72018a7dcbe1ad5d935c7715be3dd0b850c1ca6
Ryan Lieu <github-benzbrake@woai.ru>
2019-11-16 14:00:41 +0800

    修正错误构建引起的构建错误

--- hooks/build.sh
+++ hooks/build.sh
@@ -4,17 +4,18 @@ if [[ "$TRAVIS_BRANCH" != "master" ]]; then
 fi
 if [[ ${TAG_SUBDIR} = "true"  ]]; then
     ./hooks/subdir_build.sh
-fi
-# 加载 build-args
-if [[ ! -z ${BUILD_ARGS_FILE} ]] && [[ -f ${BUILD_DIRECTORY}/${BUILD_ARGS_FILE} ]]; then
-    BUILD_ARGS=$(cat ${BUILD_DIRECTORY}/${BUILD_ARGS_FILE} | sed "s#^#--build-arg #")
-fi
-# 构建映像
-docker build -t ${DOCKER_REPO}:${DOCKER_TAG-latest} ${BUILD_ARGS} ${BUILD_DIRECTORY}
-# 打 latest 标签
-if [[ ${DOCKER_TAG-latest} != "latest" ]]; then
-    if [[ ${TAG_LATEST} = "true" ]] || [[ ${TAG_LATEST} = ${DOCKER_TAG} ]]; then
-        docker tag ${DOCKER_REPO}:${DOCKER_TAG} ${DOCKER_REPO}:latest
+else
+    # 加载 build-args
+    if [[ ! -z ${BUILD_ARGS_FILE} ]] && [[ -f ${BUILD_DIRECTORY}/${BUILD_ARGS_FILE} ]]; then
+      BUILD_ARGS=$(cat ${BUILD_DIRECTORY}/${BUILD_ARGS_FILE} | sed "s#^#--build-arg #")
+    fi
+    # 构建映像
+    docker build -t ${DOCKER_REPO}:${DOCKER_TAG-latest} ${BUILD_ARGS} ${BUILD_DIRECTORY}
+    # 打 latest 标签
+    if [[ ${DOCKER_TAG-latest} != "latest" ]]; then
+        if [[ ${TAG_LATEST} = "true" ]] || [[ ${TAG_LATEST} = ${DOCKER_TAG} ]]; then
+            docker tag ${DOCKER_REPO}:${DOCKER_TAG} ${DOCKER_REPO}:latest
+        fi
     fi
 fi
 set +x
