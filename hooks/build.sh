83c4ed5437290f46f9a84f8765e5f63b2cf70f57
Ryan Lieu <github-benzbrake@woai.ru>
2019-11-16 12:00:50 +0800

    修改打 latest 标签的逻辑 transmission 镜像加入 latest 标签 修改部分注释

--- hooks/build.sh
+++ hooks/build.sh
@@ -2,13 +2,13 @@
 if [[ ${TAG_SUBDIR} = "true"  ]]; then
     ./hooks/subdir_build.sh
 fi
-# Load build args
+# 加载 build-args
 if [[ ! -z ${BUILD_ARGS_FILE} ]] && [[ -f ${BUILD_DIRECTORY}/${BUILD_ARGS_FILE} ]]; then
     BUILD_ARGS=$(cat ${BUILD_DIRECTORY}/${BUILD_ARGS_FILE} | sed "s#^#--build-arg #")
 fi
-# Build image
+# 构建映像
 docker build -t ${DOCKER_REPO}:${DOCKER_TAG} ${BUILD_ARGS} ${BUILD_DIRECTORY}
-# Tag image
-if [[ ${TAG_LATEST} = "true" ]]; then
+# 打 latest 标签
+if [[ ${DOCKER_TAG} != "latest" ]]  && [[ ${TAG_LATEST} = "true" ]] || [[ ${TAG_LATEST} = ${DOCKER_TAG} ]]; then
     docker tag ${DOCKER_REPO}:${DOCKER_TAG} ${DOCKER_REPO}:latest
 fi
