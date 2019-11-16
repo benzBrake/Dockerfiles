c9c6799d1ce3a5d36235b682baa0e8d4b6e71168
Ryan Lieu <github-benzbrake@woai.ru>
2019-11-16 11:51:13 +0800

    修正子目录构建不加载 build-args

--- hooks/subdir_build.sh
+++ hooks/subdir_build.sh
@@ -2,12 +2,15 @@
 # 去末尾 /
 BUILD_DIRECTORY=$(echo ${BUILD_DIRECTORY} | sed "s#/*\$##")
 # 列表目录
-SUB_FILES=$(\ls -d ${BUILD_DIRECTORY}/*/)
-for file in ${SUB_FILES//\/\//\/}; do
+SUB_DIRS=$(\ls -d ${BUILD_DIRECTORY}/*/)
+for SUB_DIR in ${SUB_DIRS//\/\//\/}; do
     # 获取标签
-    tag="$(echo ${file} | sed "s#/*\$##" | sed "s#.*\/##")"
+    tag="$(echo ${SUB_DIR} | sed "s#/*\$##" | sed "s#.*\/##")"
     echo "Building image: ${DOCKER_REPO}:${tag}"
-    docker build -t ${DOCKER_REPO}:${tag} ${file}
+    if [[ ! -z ${BUILD_ARGS_FILE} ]] && [[ -f ${SUB_DIR}/${BUILD_ARGS_FILE} ]]; then
+        BUILD_ARGS=$(cat ${SUB_DIR}/${BUILD_ARGS_FILE} | sed "s#^#--build-arg #")
+    fi
+    docker build -t ${DOCKER_REPO}:${tag} ${BUILD_ARGS} ${SUB_DIR}
     if [[ ${tag} = ${TAG_LATEST} ]]; then
         # 打 latest 标签
         docker tag ${DOCKER_REPO}:${tag} ${DOCKER_REPO}:latest
