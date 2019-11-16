684f5b692cb10149c2652150e57cc8f667a83a39
Ryan Lieu <github-benzbrake@woai.ru>
2019-11-16 13:28:04 +0800

    try to fix Error parsing reference

--- hooks/build.sh
+++ hooks/build.sh
@@ -12,7 +12,9 @@ fi
 # 构建映像
 docker build -t ${DOCKER_REPO}:${DOCKER_TAG-latest} ${BUILD_ARGS} ${BUILD_DIRECTORY}
 # 打 latest 标签
-if [[ ${DOCKER_TAG-latest} != "latest" ]] && [[ ${TAG_LATEST} = "true" ]] || [[ ${TAG_LATEST} = ${DOCKER_TAG} ]]; then
-    docker tag ${DOCKER_REPO}:${DOCKER_TAG} ${DOCKER_REPO}:latest
+if [[ ${DOCKER_TAG-latest} != "latest" ]]; then
+    if [[ ${TAG_LATEST} = "true" ]] || [[ ${TAG_LATEST} = ${DOCKER_TAG} ]]; then
+        docker tag ${DOCKER_REPO}:${DOCKER_TAG} ${DOCKER_REPO}:latest
+    fi
 fi
 set +x
