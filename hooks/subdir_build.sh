d0f021f03f8479d1abe1f249d5cca0a1d5bda9c6
Ryan Lieu <github-benzbrake@woai.ru>
2019-11-16 11:40:34 +0800

    Fix subdir_build.sh not found

---
+++ hooks/subdir_build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# 去末尾 /
+BUILD_DIRECTORY=$(echo ${BUILD_DIRECTORY} | sed "s#/*\$##")
+# 列表目录
+SUB_FILES=$(\ls -d ${BUILD_DIRECTORY}/*/)
+for file in ${SUB_FILES//\/\//\/}; do
+    # 获取标签
+    tag="$(echo ${file} | sed "s#/*\$##" | sed "s#.*\/##")"
+    echo "Building image: ${DOCKER_REPO}:${tag}"
+    docker build -t ${DOCKER_REPO}:${tag} ${file}
+    if [[ ${tag} = ${TAG_LATEST} ]]; then
+        # 打 latest 标签
+        docker tag ${DOCKER_REPO}:${tag} latest
+    fi
+done
