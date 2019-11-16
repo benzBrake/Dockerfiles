82a241ce1a94291b5c3f87661ff08fbac8db09dd
Ryan Lieu <github-benzbrake@woai.ru>
2019-11-16 13:21:49 +0800

    fuck

--- hooks/build.sh
+++ hooks/build.sh
@@ -12,7 +12,7 @@ fi
 # 构建映像
 docker build -t ${DOCKER_REPO}:${DOCKER_TAG-latest} ${BUILD_ARGS} ${BUILD_DIRECTORY}
 # 打 latest 标签
-if [[ ${DOCKER_TAG-latest}!= "latest" ]] && [[ ${TAG_LATEST} = "true" ]] || [[ ${TAG_LATEST} = ${DOCKER_TAG} ]]; then
+if [[ ${DOCKER_TAG-latest} != "latest" ]] && [[ ${TAG_LATEST} = "true" ]] || [[ ${TAG_LATEST} = ${DOCKER_TAG} ]]; then
     docker tag ${DOCKER_REPO}:${DOCKER_TAG} ${DOCKER_REPO}:latest
 fi
 set +x
