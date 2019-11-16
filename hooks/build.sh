c9d5962c263c328c3df958b6239cac996b4fde70
Ryan Lieu <github-benzbrake@woai.ru>
2019-11-16 14:02:14 +0800

    合并重复 build-args 加载代码

--- hooks/build.sh
+++ hooks/build.sh
@@ -2,13 +2,13 @@
 if [[ "$TRAVIS_BRANCH" != "master" ]]; then
     set -x
 fi
+# 加载 build-args
+if [[ ! -z ${BUILD_ARGS_FILE} ]] && [[ -f ${BUILD_DIRECTORY}/${BUILD_ARGS_FILE} ]]; then
+    export BUILD_ARGS=$(cat ${BUILD_DIRECTORY}/${BUILD_ARGS_FILE} | sed "s#^#--build-arg #")
+fi
 if [[ ${TAG_SUBDIR} = "true"  ]]; then
     ./hooks/subdir_build.sh
 else
-    # 加载 build-args
-    if [[ ! -z ${BUILD_ARGS_FILE} ]] && [[ -f ${BUILD_DIRECTORY}/${BUILD_ARGS_FILE} ]]; then
-      BUILD_ARGS=$(cat ${BUILD_DIRECTORY}/${BUILD_ARGS_FILE} | sed "s#^#--build-arg #")
-    fi
     # 构建映像
     docker build -t ${DOCKER_REPO}:${DOCKER_TAG-latest} ${BUILD_ARGS} ${BUILD_DIRECTORY}
     # 打 latest 标签
--- hooks/subdir_build.sh
+++ hooks/subdir_build.sh
@@ -10,9 +10,6 @@ for SUB_DIR in ${SUB_DIRS//\/\//\/}; do
     # 获取标签
     tag="$(echo ${SUB_DIR} | sed "s#/*\$##" | sed "s#.*\/##")"
     echo "Building image: ${DOCKER_REPO}:${tag}"
-    if [[ ! -z ${BUILD_ARGS_FILE} ]] && [[ -f ${SUB_DIR}/${BUILD_ARGS_FILE} ]]; then
-        BUILD_ARGS=$(cat ${SUB_DIR}/${BUILD_ARGS_FILE} | sed "s#^#--build-arg #")
-    fi
     docker build -t ${DOCKER_REPO}:${tag} ${BUILD_ARGS} ${SUB_DIR}
     if [[ ${tag} = ${TAG_LATEST} ]]; then
         # 打 latest 标签
