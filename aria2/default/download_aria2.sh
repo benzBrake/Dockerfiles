055a483c37f4624df8c6f0e9dcadddaa7f058a72
Ryan Lieu <github-benzbrake@woai.ru>
2019-11-19 10:16:37 +0800

    尝试修复找不到index.html

--- aria2/default/download_aria2.sh
+++ aria2/default/download_aria2.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Download Aria2
 if [[ ${BUILD_VERSION} = "latest" ]]; then
     _LINK="https://github.com$(curl -sL -o - https://github.com/q3aql/aria2-static-builds/releases/${BUILD_VERSION} | grep "aria2-.*-linux-gnu-64bit-build1.tar.bz2" | grep "<a" | awk -F '"' '{print $2}' | head -n1)"
 else
@@ -15,3 +16,7 @@ tar xf aria2*.tar && \
 rm -rf aria2*.tar && \
 mv aria2* aria2
 chmod +x /aria2/aria2c
+# Download Aira2ng
+curl -sSL https://github.com$(curl -sSL -o - https://github.com/mayswind/AriaNg/releases/latest | grep "AriaNg-[^/]*-AllInOne.zip" | grep "<a" | awk -F'"' '{print $2}') -o Aria2ng.zip
+unzip Aria2ng.zip
+mv index.html /data/
